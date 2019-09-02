0638ead56555c2fc8094350bc280f4fea2b14436
wuzhensheng <zhensheng.five@gmail.com>
2019-09-02 11:49:33 +0800

    更改oss位置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? 'https://xgxw.oss-cn-beijing.aliyuncs.com/public/dist'
+        ? 'https://xgxw-cdn.oss-cn-beijing.aliyuncs.com/xgxw'
         : '/'
 }
 
