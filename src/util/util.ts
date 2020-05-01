e8a378a26c7e7e531791b229a646eb2a47b38395
wzs <zhensheng.five@gmail.com>
2020-05-01 10:11:14 +0800

    v2, 使用新的设计. 原先的清空重写

--- src/util/util.ts
+++
@@ -1,8 +0,0 @@
-import { MobileWidth } from "@/constants/constants";
-
-export const isMobile = function () {
-  if (document.body.clientWidth < MobileWidth) {
-    return true;
-  }
-  return false;
-};
